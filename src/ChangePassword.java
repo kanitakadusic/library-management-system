d339ddcbe66e5356e0a6fcb2246b241240d130c1
kanitakadusic <kanita.kadusic@treca-gimnazija.edu.ba>
2022-04-02 16:02:38 +0200

    Password Validation

--- src/ChangePassword.java
+++ src/ChangePassword.java
@@ -3,6 +3,8 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.io.Serial;
 import java.sql.*;
+import java.util.HashMap;
+import java.util.regex.Pattern;
 
 public class ChangePassword extends JFrame {
     @Serial
@@ -45,26 +47,58 @@ public class ChangePassword extends JFrame {
 
         submit.addActionListener(e -> {
             String password = passwordField.getText();
+            String validationMessage = passwordValidationMessage(password);
 
-            try {
-                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "");
+            if (validationMessage.isEmpty()) {
+                try {
+                    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "");
 
-                PreparedStatement preparedStatement = connection.prepareStatement("UPDATE users SET password = ? WHERE userID = ?");
-                preparedStatement.setString(1, password);
-                preparedStatement.setString(2, userID);
-                preparedStatement.executeUpdate();
+                    PreparedStatement preparedStatement = connection.prepareStatement("UPDATE users SET password = ? WHERE userID = ?");
+                    preparedStatement.setString(1, password);
+                    preparedStatement.setString(2, userID);
+                    preparedStatement.executeUpdate();
 
-                JLabel message = new JLabel(" Password has been successfully changed ");
+                    JLabel message = new JLabel(" Password has been successfully changed ");
+                    message.setFont(new Font("Arial", Font.PLAIN, 18));
+                    message.setForeground(new Color(92, 184, 92));
+                    JOptionPane.showOptionDialog(null, message, "Password changed", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new Object[]{}, null);
+
+                    dispose();
+                } catch (SQLException sqlException) {
+                    sqlException.printStackTrace();
+                }
+            } else {
+                JLabel message = new JLabel(validationMessage);
                 message.setFont(new Font("Arial", Font.PLAIN, 18));
-                message.setForeground(new Color(92, 184, 92));
-                JOptionPane.showOptionDialog(null, message, "Password changed", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new Object[]{}, null);
-
-                dispose();
-            } catch (SQLException sqlException) {
-                sqlException.printStackTrace();
+                message.setForeground(new Color(217, 83, 79));
+                JOptionPane.showOptionDialog(null, message, "Password validation message", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new Object[]{}, null);
             }
         });
 
         panel.add(submit);
+    }
+
+    public static String passwordValidationMessage(String password) {
+        StringBuilder message = new StringBuilder("<html><body>");
+
+        HashMap<String, String> validation = new HashMap<>();
+        validation.put("^(?=.*[a-z]).{1,}$", "add lowercase letter");
+        validation.put("^(?=.*[A-Z]).{1,}$", "add uppercase letter");
+        validation.put("^(?=.*[0-9]).{1,}$", "add number");
+        validation.put("^(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{1,}$", "add special character");
+        validation.put("^(?=\\S+$).{1,}$", "remove spaces");
+        validation.put("^.{8,20}$", "limit 8-20 characters");
+
+        for (String i : validation.keySet())
+            if (!Pattern.compile(i).matcher(password).matches())
+                message.append("&nbsp;•&nbsp;&nbsp;").append(validation.get(i)).append("<br>");
+
+        message.append("</body></html>");
+
+        if (message.toString().equals("<html><body></body></html>")) {
+            return "";
+        }
+
+        return message.toString();
     }
 }
